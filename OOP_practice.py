9eb958c99357771e9d882c6a7ba03cea12c0b12c
Lloyd Aboulnaja <lloydaboulnaja@gmail.com>
2026-07-26 20:05:06 +0100

    Rename Tv class to Cart_item and add item handling
    Refactor Tv class to Cart_item and implement item addition logic.

--- OOP_practice.py
+++ OOP_practice.py
@@ -1,7 +1,7 @@
 
 
 
-class Tv:
+class Cart_item:
     def __init__(self,model,price):
         self.model = model
         self.price = price
@@ -31,20 +31,64 @@ class Cart:
 
 
 
+def get_details():
+
+    while True:
+        model = input("\nEnter Model:")
+
+        if len(model) < 1:
+            print("Please enter a valid item name!")
+        elif model.isdigit():
+            print("The item cannot be a number!")
+        else:
+            break
+
+        #price = input("Ente)r price ($):")
+
+
+Store = Cart("Amazon",[])
+
+def main():
+    flag = True
+
+    print("#"*20)
+    print("\n## 1. Add Items")
+    print("## 2. View Cart")
+    print("## 3. Remove Items")
+    print("## 4. View Wallet")
+    print("## 5. Exit Program")
+    print("")
+    print("#"*20)
+
+    while flag:
+        try:
+            choice = int(input("Enter a choice from the options above: "))
+        except ValueError:
+            print("Your choice Must be a number (1-4) ")
+            continue
+
+        if choice not in [1,2,3,4]:
+            print("Invalid choice!.")
+
+        elif choice == 1:
+            get_details()
+
+        else:
+            break
+
+      
 
 
 
-amazon = Cart("Amazon",[])
+#model = input("Enter Model:")
+#price = input("Enter price ($):")
+
+#brand = Cart_item(model,price)
+
+#store.items.append(brand.model)
+
+#print(store.shopping_cart_items())
 
 
-
-model = input("model:")
-price = input("price:")
-
-brand = Tv(model,price)
-
-amazon.items.append(brand.model)
-
-print(amazon.shopping_cart_items())
-
-
+if __name__ == "__main__":
+    main()
